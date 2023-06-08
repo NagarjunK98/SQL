c10a9d5357b44bf6da9a897a705f09e30464eb56
NagarjunK98 <53399371+nagarjunkumar@users.noreply.github.com>
2023-06-08 08:01:19 +0530

    added hackerrank sql solved questions

---
+++ hackerrank_solved.sql
@@ -0,0 +1,24 @@
+'''
+1. Interviews https://www.hackerrank.com/challenges/interviews/submissions/code/319331270
+
+2. The PADS https://www.hackerrank.com/challenges/the-pads/submissions/code/146209124
+
+3. New Companies https://www.hackerrank.com/challenges/the-company/submissions/code/164122828
+
+4. Weather observation station 18 https://www.hackerrank.com/challenges/weather-observation-station-18/submissions/code/145809968
+
+5. Weather observation station 19 https://www.hackerrank.com/challenges/weather-observation-station-19/submissions/code/145812010
+
+6. The Report https://www.hackerrank.com/challenges/the-report/submissions/code/164122509
+
+7. Top Competitors https://www.hackerrank.com/challenges/full-score/submissions/code/319291011
+
+8. Ollivanders Inventory https://www.hackerrank.com/challenges/harry-potter-and-wands/submissions/code/319524074
+
+9. Challenges https://www.hackerrank.com/challenges/challenges/submissions/code/319321509
+
+10. Context board https://www.hackerrank.com/challenges/contest-leaderboard/submissions/code/319484571
+
+11. Placements https://www.hackerrank.com/challenges/placements/submissions/code/319289020
+
+'''
